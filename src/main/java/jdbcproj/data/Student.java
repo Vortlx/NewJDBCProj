398e9f0396b343032d642fe9675d77191f618738
Alexander Lebedev <jkdsgjksdjg@gmail.com>
2016-10-20 21:26:16 +0300

    Student hibernate mapping. Deleted comment from getters and setters.

--- src/main/java/jdbcproj/data/Student.java
+++ src/main/java/jdbcproj/data/Student.java
@@ -1,5 +1,8 @@
 package jdbcproj.data;
 
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 /**
  * This class describe student.
  *
@@ -8,43 +11,24 @@ package jdbcproj.data;
  * */
 public class Student extends Person {
 
+    @ManyToOne
+    @JoinColumn(name="id_group")
     private Group group;
 
-    /**
-     * This constructor create undefined student.
-     * */
     public Student(){
         super();
         group = new Group();
     }
 
-    /**
-     * This constructor create specific student.
-     *
-     * @param name Name of a student
-     * @param familyName Family name of student
-     * @param group Group in which student is
-     * */
     public Student(int id, String name, String familyName, Group group){
         super(id, name, familyName);
         this.group = group;
     }
 
-    /**
-     * This method return number of group in which student is.
-     *
-     * @return int Number of group in which student is
-     * */
     public Group getGroup() {
         return group;
     }
 
-    /**
-     * This method change number of group in which student is on new number.
-     *
-     * @param group Number of new group
-     * @return Nothing
-     * */
     public void setGroup(Group group) {
         this.group = group;
     }
