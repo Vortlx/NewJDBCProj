f181057dceb72d5b723d3c0bcdb343114b3a6a5e
Alexander Lebedev <jkdsgjksdjg@gmail.com>
2016-10-20 21:23:23 +0300

    Person hibernate mapping. Deleted comment from getters and setters.

--- src/main/java/jdbcproj/data/Person.java
+++ src/main/java/jdbcproj/data/Person.java
@@ -1,5 +1,8 @@
 package jdbcproj.data;
 
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 
 /**
  * This abstract class describe a person. For example student or teacher.
@@ -7,76 +10,45 @@ package jdbcproj.data;
  * @author Lebedev Alexander
  * @since 2016-09-19
  * */
+@MappedSuperclass
 public abstract class Person {
 	
+    @Id
+    @Column(name="id")
 	private int id;
+    
+    @Column(name="name")
 	private String name;
+    
+    @Column(name="family_name")
 	private String familyName;
 	
-	/**
-	 * This constructor create undefined person. 
-	 * */
 	public Person(){
-		id = -1;
-		name = "";
-		familyName = "";
+		
 	}
 	
-	/**
-	 * This constructor create person which have specific name and family name
-	 * 
-	 * @param name Name of person
-	 * @param familyName Family name of person
-	 * */
 	public Person(int id, String name, String familyName){
 		this.id = id;
 		this.name = name;
 		this.familyName = familyName;
 	}
 
-	/**
-	 * Method return id of person
-	 *
-	 * @return int ID of person
-	 * */
 	public int getId() {
 		return id;
 	}
 
-	/**
-	 * This method return name of person
-	 * 
-	 * @return String Name of Person
-	 * */
 	public String getName() {
 		return name;
 	}
 	
-	/**
-	 * This method change current name of person on new name.
-	 * 
-	 * @param name New name of person
-	 * @return Nothing
-	 * */
 	public void setName(String name) {
 		this.name = name;
 	}
 	
-	/**
-	 * This method return family name of person
-	 * 
-	 * @return String Family name of person
-	 * */
 	public String getFamilyName() {
 		return familyName;
 	}
 	
-	/**
-	 * This method change current family name of person on new family name.
-	 * 
-	 * @param familyName New  family name of person
-	 * @return Nothing
-	 * */
 	public void setFamilyName(String familyName) {
 		this.familyName = familyName;
 	}
